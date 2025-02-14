bcb315ed3bdcabce2fb9bde5b31c764f0ca5ee0e
Ranel <67388819+Ranel435@users.noreply.github.com>
2025-02-14 15:05:28 +0300

    main & utils added

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int sum(int a, int b);
+int max(int a, int b);
+int min(int a, int b);
+int sqrt(int a);
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "I am compiled!" << std::endl;
+
+    int x = 1234, y = 123;
+
+    std::cout << "sum: " << max(x, y) << std::endl;
+    std::cout << "sqrt: " << sqrt(x) << std::endl;
+
+    std::cout << "max: " << max(x, y) << std::endl;
+    std::cout << "min: " << min(x, y) << std::endl;
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ src/utils.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int sqrt(int a) {
+    return a * a;
+}
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
