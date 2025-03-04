4083442d46fdd2d8600dd5253efad1d12ed1bf2c
Ranel <67388819+Ranel435@users.noreply.github.com>
2025-03-05 00:21:06 +0300

    test1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int sum(int a, int b);
+int plus(int a, int b);
 int max(int a, int b);
 int min(int a, int b);
 int sqrt(int a);
@@ -13,7 +13,7 @@ int main(int argc, char const *argv[])
 
     int x = 1234, y = 123;
 
-    std::cout << "sum: " << sum(x, y) << std::endl;
+    std::cout << "sum: " << plus(x, y) << std::endl;
     std::cout << "sqrt: " << sqrt(x) << std::endl;
 
     std::cout << "max: " << max(x, y) << std::endl;
